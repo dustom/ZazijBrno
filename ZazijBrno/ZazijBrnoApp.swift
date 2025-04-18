95b1153d2b53e4106eaca2ec1127fd1c88c09f8d
dustom <dusek.tomas@icloud.com>
2025-04-18 10:43:33 +0200

    Initial Commit

---
+++ ZazijBrno/ZazijBrnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZazijBrnoApp.swift
+//  ZazijBrno
+//
+//  Created by Tomáš Dušek on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZazijBrnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
